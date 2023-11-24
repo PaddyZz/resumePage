d1c602357c7b0c8ce37e27c763d7482b24e12bef
Jiahe(Paddy) ZHAO <86382862+PaddyZz@users.noreply.github.com>
2023-11-24 18:23:46 +0800

    Update style.js

--- style.js
+++ style.js
@@ -5,3 +5,30 @@ var typed = new Typed("#nameText",{
     backDelay:1000,
     loop:true
 });
+
+
+setTimeout(function() {
+    var newTyped = new Typed("#majorText", {
+      strings: ["AI","Web Development"],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 1000,
+      loop: true
+    });
+  }, 2000); 
+
+
+  const myInput = document.getElementById('button');
+  
+  myInput.addEventListener('click', function() {
+    const downloadURL = "https://github.com/PaddyZz/resumePage/raw/main/src/xuemei.zip";
+    
+    
+    const link = document.createElement('a');
+    link.href = downloadURL;
+    link.download = 'hottieXueMei.zip'; 
+    
+    
+    link.click();
+  });
+ 
