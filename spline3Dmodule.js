303a13a7a69848a5c18fdfe77fcdb938ff2b48a8
Jiahe(Paddy) ZHAO <86382862+PaddyZz@users.noreply.github.com>
2024-01-21 21:21:06 +0800

    Add files via upload

---
+++ spline3Dmodule.js
@@ -0,0 +1,8 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+/*
+app.load('https://prod.spline.design/uVspaCtsHxKcuXHn/scene.splinecode');
+*/
+app.load('https://draft.spline.design/6CAVJN5uOzHPc4Av/scene.splinecode');
