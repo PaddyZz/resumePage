da8c12598d264c912b6a90c243c44c94dc99a013
Jiahe(Paddy) ZHAO <86382862+PaddyZz@users.noreply.github.com>
2024-02-20 00:00:53 +0800

    Delete spline3Dmodule.js

--- spline3Dmodule.js
+++
@@ -1,8 +0,0 @@
-import { Application } from '@splinetool/runtime';
-
-const canvas = document.getElementById('canvas3d');
-const app = new Application(canvas);
-/*
-app.load('https://prod.spline.design/uVspaCtsHxKcuXHn/scene.splinecode');
-*/
-app.load('https://draft.spline.design/6CAVJN5uOzHPc4Av/scene.splinecode');
